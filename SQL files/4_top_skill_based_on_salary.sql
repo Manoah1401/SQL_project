e2dea2714af4d0f327aace98ba77a498a32cc413
Manoah1401 <manoahnob@gmail.com>
2026-08-15 20:01:36 +0530

    Top skills based on salary

---
+++ SQL files/4_top_skill_based_on_salary.sql
@@ -0,0 +1,22 @@
+-- To find the top skill based on salary
+-- Sort the Data analyst jobs based on salary
+-- The skills required for these jobs
+-- Find the average salary per skill
+-- Why? => To understand the ROI for learning each skill
+
+SELECT 
+    skills,
+    ROUND(AVG(salary_year_avg),2) as Average_Salary
+FROM
+    job_postings_fact AS jbf
+    INNER JOIN skills_job_dim as sjd
+    on jbf.job_id = sjd.job_id
+    INNER JOIN skills_dim as sd
+    on sjd.skill_id = sd.skill_id
+WHERE
+    job_title_short = 'Data Analyst' and salary_year_avg is NOT NULL
+GROUP BY
+    skills
+ORDER BY
+    Average_Salary DESC
+LIMIT 5
---
+++ SQL files/5_Most_Optimal_Skills.sql
@@ -0,0 +1,2 @@
+-- Most Optimal skills corresponding to the most amount of opportunities as well being the highest paid.
+
