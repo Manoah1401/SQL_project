31fe61bd8840acb5f44091b1542af270ca760ef1
Manoah1401 <manoahnob@gmail.com>
2026-08-17 23:14:48 +0530

    Optimal Skills to learn

--- SQL files/5_Most_Optimal_Skills.sql
+++ SQL files/5_Most_Optimal_Skills.sql
@@ -1,2 +1,29 @@
 -- Most Optimal skills corresponding to the most amount of opportunities as well being the highest paid.
 
+-- SELECT * FROM job_postings_fact ;
+-- SELECT * FROM company_dim LIMIT 10;
+-- SELECT * FROM skills_dim LIMIT 10;
+-- SELECT * FROM skills_job_dim LIMIT 10;
+
+
+SELECT 
+    sd.skill_id,
+    sd.skills,
+    Count(jbf.job_id) as opportunities,
+    ROUND(AVG(salary_year_avg),2) AS Avg_salary
+FROM job_postings_fact AS jbf
+    LEFT JOIN company_dim AS cd
+    ON jbf.company_id = cd.company_id
+    INNER JOIN skills_job_dim as sjd
+    ON jbf.job_id = sjd.job_id
+    LEFT JOIN skills_dim AS sd
+    ON sjd.skill_id = sd.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL
+
+GROUP BY sd.skill_id
+HAVING
+    Count(jbf.job_id)>10
+ORDER BY
+    opportunities DESC,
+    Avg_salary DESC;
